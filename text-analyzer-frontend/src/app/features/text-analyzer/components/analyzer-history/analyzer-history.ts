fa12bc24dd0d7d635ed46cc696b375539e549ed8
Darius Bozkaya <darboviedev@gmail.com>
2025-09-01 16:42:00 +0200

    added history feature

--- text-analyzer-frontend/src/app/features/text-analyzer/components/analyzer-history/analyzer-history.ts
+++ text-analyzer-frontend/src/app/features/text-analyzer/components/analyzer-history/analyzer-history.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {AnalysisResult} from '../../models/interfaces/analysis-result';
+import {ResultProvider} from '../../services/result-provider';
+import {AsyncPipe, KeyValuePipe} from '@angular/common';
 
 @Component({
   selector: 'app-analyzer-history',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    KeyValuePipe
+  ],
   templateUrl: './analyzer-history.html',
   styleUrl: './analyzer-history.css'
 })
-export class AnalyzerHistory {
+export class AnalyzerHistory implements OnInit{
 
+  history$: Observable<AnalysisResult[]> ;
+
+
+  constructor(private analyzerResultProvider: ResultProvider) {
+    this.history$ = this.analyzerResultProvider.history$;
+  }
+
+  ngOnInit() {}
 }
