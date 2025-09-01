748886898deca33d2261ff22237bad771f94fddd
Darius Bozkaya <darboviedev@gmail.com>
2025-09-01 12:24:33 +0200

    added Material UI

---
+++ text-analyzer-frontend/src/app/shared/material-imports.ts
@@ -0,0 +1,26 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonToggleGroup} from '@angular/material/button-toggle';
+import {MatButtonToggle} from '@angular/material/button-toggle';
+import {MatSelectModule} from '@angular/material/select';
+
+export const MaterialImports = [
+  MatToolbarModule,
+  MatCardModule,
+  MatButtonModule,
+  MatSlideToggleModule,
+  MatInputModule,
+  MatTableModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatButtonToggleGroup,
+  MatButtonToggle,
+  MatSelectModule
+
+];
