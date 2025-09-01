ca790ec16f9cedb7e7477fb1eba17556557534ac
Darius Bozkaya <darboviedev@gmail.com>
2025-09-01 13:01:03 +0200

    added html scaffold for analyzer-home

--- text-analyzer-frontend/src/app/app.ts
+++ text-analyzer-frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AnalyzerHome} from './features/text-analyzer/components/analyzer-home/analyzer-home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AnalyzerHome],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- text-analyzer-frontend/src/app/features/text-analyzer/components/analyzer-home/analyzer-home.ts
+++ text-analyzer-frontend/src/app/features/text-analyzer/components/analyzer-home/analyzer-home.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {AnalyzerInput} from '../analyzer-input/analyzer-input';
+import {AnalyzerResult} from '../analyzer-result/analyzer-result';
+import {AnalyzerHistory} from '../analyzer-history/analyzer-history';
+import {MaterialImports} from '../../../../shared/material-imports';
 
 @Component({
   selector: 'app-analyzer-home',
-  imports: [],
+  imports: [AnalyzerInput,
+    AnalyzerResult,
+    AnalyzerHistory,
+    MaterialImports],
   templateUrl: './analyzer-home.html',
   styleUrl: './analyzer-home.css'
 })
